ac2a97db76ea317fb2dc2c232ea923685b92fc7e
Bruno <brunocesar.tenorio1@gmail.com>
2023-08-21 15:25:52 -0300

    fix in scroll animation

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,13 +1,13 @@
 export default function initScrollAnimation() {
   const sections = document.querySelectorAll("[data-scroll-animation]");
   if (sections.length) {
-    function showSection() {
-      const screenHalf = window.innerHeight * 0.6;
+    const screenHalf = window.innerHeight * 0.6;
+    function activeSection(event) {
       sections.forEach((section) => {
         const isVisible = section.getBoundingClientRect().top - screenHalf < 0;
         section.classList.toggle("active", isVisible);
       });
     }
-    window.addEventListener("scroll", showSection);
+    window.addEventListener("scroll", activeSection);
   }
 }
