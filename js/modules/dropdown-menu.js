263ac5f74011199cdaa7338d467aa4bb3dfdca95
Bruno <brunocesar.tenorio1@gmail.com>
2023-11-09 11:42:00 -0300

    fixing dropdown menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,12 +1,7 @@
 import initOutsideClick from "./outsideclick.js";
 
-<<<<<<< HEAD
-export default function dropdownMenu() {
-  const menu = document.querySelector("[data-dropdown]");
-=======
 export default function initDropdownMenu() {
   const menu = document.querySelector("[data-dropdown-menu]");
->>>>>>> 8daebf73068f13b99fc0003f2f7dc63e81380efd
   if (menu) {
     ["touchstart", "click"].forEach((userEvent) => {
       menu.addEventListener(userEvent, showDropdownMenu);
