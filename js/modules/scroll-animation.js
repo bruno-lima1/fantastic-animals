aad084156c8de3387b1b9afd1e00fb6276c5c838
Bruno César <brunocesar.tenorio1@gmail.com>
2023-12-21 17:30:17 -0300

    updating scroll animation

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -2,11 +2,11 @@ export default function initScrollAnimation() {
   const sections = document.querySelectorAll("[data-scroll-animation]");
   if (sections.length) {
     const screenHalf = window.innerHeight * 0.6;
-    window.addEventListener("scroll", activeSection);
-    function activeSection() {
+    window.addEventListener("scroll", showSection);
+    function showSection() {
       sections.forEach((section) => {
-        const isVisible = section.getBoundingClientRect().top - screenHalf < 0;
-        section.classList.toggle("active", isVisible);
+        const sectionIsVisible = section.getBoundingClientRect().top - screenHalf < 0;
+        section.classList.toggle("active", sectionIsVisible);
       });
     }
   }
