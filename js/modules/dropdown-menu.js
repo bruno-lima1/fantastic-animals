d87d2cd6449bd004e74c76a568799777d47da8ec
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-09 14:15:27 -0300

    updating dropdown menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,15 +1,15 @@
-import initOutsideClick from "./outsideclick.js";
+import initOutsideClick from "./outsideclick.js"
 
 export default function initDropdownMenu() {
   const menu = document.querySelector("[data-dropdown-menu]");
   if (menu) {
-    menu.addEventListener("click", showDropdownMenu);
-    function showDropdownMenu(event) {
+    menu.addEventListener("click", handleClick);
+    function handleClick(event) {
       event.preventDefault();
       menu.classList.toggle("active");
-      initOutsideClick(menu, () => {
-        menu.classList.remove("active");
-      });
+      initOutsideClick(menu, "click", () => {
+      menu.classList.remove("active");
+      })
     }
   }
 }
--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -1,14 +1,14 @@
-export default function initOutsideClick(element, callback) {
+export default function initOutsideClick(element, click, callback) {
   const html = document.documentElement;
   const outside = "data-outside";
   if (!element.hasAttribute(outside)) {
-    setTimeout(() => html.addEventListener("click", hideDropdownMenu));
+    html.addEventListener("click", handleClick);
     element.setAttribute(outside, "");
   }
-  function hideDropdownMenu(event) {
+  function handleClick(event) {
     if (!element.contains(event.target)) {
+      html.removeEventListener("click", handleClick);
       element.removeAttribute(outside);
-      html.removeEventListener("click", hideDropdownMenu);
       callback();
     }
   }
