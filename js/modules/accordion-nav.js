cabe8eced16e8309345b0dbfe991bc3b1c7280d6
Bruno César <brunocesar.tenorio1@gmail.com>
2024-01-09 15:48:56 -0300

    updating accordion nav

--- js/modules/accordion-nav.js
+++ js/modules/accordion-nav.js
@@ -1,16 +1,16 @@
 export default function initAccordionNav() {
   const askings = document.querySelectorAll("[data-accordion-nav] button");
   const responses = document.querySelectorAll("[data-accordion-nav] dd");
-  if (askings.length) {
+  if (askings.length && responses.length) {
     responses[0].classList.add("active");
     askings.forEach((asks) => {
       asks.addEventListener("click", handleClick);
-      function handleClick() {
-        const controls = asks.getAttribute("aria-controls");
+      function handleClick(event) {
+        const controls = event.target.getAttribute("aria-controls");
         const response = document.getElementById(controls);
         response.classList.toggle("active");
         const active = response.classList.contains("active");
-        asks.setAttribute("aria-expanded", active)
+        asks.setAttribute("aria-expanded", active);
       }
     });
   }
