6d80142e6c9255d7b21022677e0a8d8c16ab42bf
Bruno César <brunocesar.tenorio1@gmail.com>
2024-02-08 13:00:10 -0300

    updating js

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,15 +1,15 @@
 export default function initFetchBitcoin() {
   const bitcoinValue = document.querySelector("[data-bitcoin]");
   if (bitcoinValue) {
-    async function showBitcoinValue() {
+    async function bitcoinApi() {
       try {
         const response = await fetch("https://blockchain.info/ticker");
         const data = await response.json();
-        bitcoinValue.innerHTML = (100 / data.BRL.sell).toFixed(4);
+        bitcoinValue.innerText = (100 / data.BRL.sell).toFixed(4);
       } catch (erro) {
         console.log(erro);
       }
     }
-    showBitcoinValue();
+    bitcoinApi();
   }
 }
