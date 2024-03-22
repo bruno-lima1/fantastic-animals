99beb1829bbbd377c4956fc043a9970cb0aff20b
Bruno César <brunocesar.tenorio1@gmail.com>
2024-03-22 17:16:07 -0300

    chanching smooth scroll to class

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -1,17 +1,28 @@
-export default function initSmoothScroll() {
-  const links = document.querySelectorAll("[data-smooth-scroll] a[href^='#']");
-  if (links.length) {
-    links.forEach((link) => {
-      link.addEventListener("click", handleClick);
-      function handleClick(event) {
-        event.preventDefault();
-        const href = event.target.getAttribute("href");
-        const section = document.querySelector(href);
-        section.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
-      }
+export default class SmoothScroll {
+  constructor(links, options) {
+    this.internalLinks = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    this.activeScroll = this.activeScroll.bind(this);
+  }
+  activeScroll(event) {
+    event.preventDefault();
+    const href = event.target.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+  addLinkEvent() {
+    this.internalLinks.forEach((link) => {
+      link.addEventListener("click", this.activeScroll);
     });
+  }
+  init() {
+    if (this.internalLinks.length) {
+      this.addLinkEvent();
+    }
+    return this
   }
 }
