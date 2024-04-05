2f216afe659d854c46776bcbf77fbfb13261da9c
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-05 12:20:06 -0300

    modal review

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,17 +1,71 @@
 export default function initModal() {
-  const open = document.querySelector("[data-modal-open]");
-  const close = document.querySelector("[data-modal-close]");
-  const container = document.querySelector("[data-modal-container]");
+  const open = document.querySelector("[data-modal-open]")
+  const close = document.querySelector("[data-modal-close]")
+  const container = document.querySelector("[data-modal-container]")
+  function toggleModal(event) {
+    event.preventDefault();
+    container.classList.toggle("active")
+  }
+  function clickOutsideModal(event) {
+    return event.target === this ? toggleModal(event) : undefined;
+  }
   if (open && close && container) {
-    open.addEventListener("click", handleClick);
-    close.addEventListener("click", handleClick);
-    container.addEventListener("click", hideModal);
-    function handleClick(event) {
-      event.preventDefault();
-      container.classList.toggle("active")
-    }
-    function hideModal(event) {
-      return event.target === this ? container.classList.remove("active") : undefined;
-    }
+  open.addEventListener("click", toggleModal)
+  close.addEventListener("click", toggleModal)
+  container.addEventListener("click", clickOutsideModal)
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default class Modal {
+//   constructor(open, close, container) {
+//     this.open = document.querySelector(open);
+//     this.close = document.querySelector(close);
+//     this.container = document.querySelector(container);
+//     this.eventToggleModal = this.eventToggleModal.bind(this);
+//     this.clickOutsideModal = this.clickOutsideModal.bind(this);
+//   }
+//   clickOutsideModal(event) {
+//     return event.target === this.container
+//       ? this.toggleModal()
+//       : undefined;
+//   }
+//   toggleModal() {
+//     this.container.classList.toggle("active");
+//   }
+//   eventToggleModal(event) {
+//     event.preventDefault();
+//     this.toggleModal()
+//   }
+//   addEvent() {
+//     this.open.addEventListener("click", this.eventToggleModal);
+//     this.close.addEventListener("click", this.eventToggleModal);
+//     this.container.addEventListener("click", this.clickOutsideModal);
+//   }
+//   init() {
+//     if (this.open && this.close && this.container) {
+//       this.addEvent();
+//     }
+//     return this;
+//   }
+// }
--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
 import initAccordionNav from "./modules/accordion-nav.js";
 import initScrollAnimation from "./modules/scroll-animation.js";
-import initModal from "./modules/modal.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initTimeOperation from "./modules/time-operation.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -13,9 +13,9 @@ import initNumberAnimation from "./modules/number-animation.js";
 
 initSmoothScroll()
 initTabNav()
+initModal();
 initAccordionNav();
 initScrollAnimation();
-initModal();
 initDropdownMenu();
 initTimeOperation();
 initMenuMobile();
@@ -38,6 +38,9 @@ initNumberAnimation();
 
 
 // import TabNav from "./modules/tab-nav.js";
-
 // const tabNav = new TabNav("[data-tab-menu] img", "[data-tab-content] li")
 // tabNav.init()
+
+// import Modal from "./modules/modal.js";
+// const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]")
+// modal.init()
