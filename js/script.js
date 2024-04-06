7492204c972d791a84b09275261049b1b8c367d4
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-06 09:11:38 -0300

    number animation review

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
-import initNumberAnimation from "./modules/number-animation.js";
+// import initNumberAnimation from "./modules/number-animation.js";
 import initAccordionNav from "./modules/accordion-nav.js";
 import initScrollAnimation from "./modules/scroll-animation.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -14,7 +14,7 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 initSmoothScroll()
 initTabNav()
 initModal();
-initNumberAnimation();
+// initNumberAnimation();
 initAccordionNav();
 initScrollAnimation();
 initDropdownMenu();
@@ -45,6 +45,6 @@ initFetchBitcoin();
 // const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]")
 // modal.init()
 
-// import NumberAnimation from "./modules/number-animation.js";
-// const numberAnimation = new NumberAnimation(".numeros span", ".numeros", "active")
-// numberAnimation.init()
+import NumberAnimation from "./modules/number-animation.js";
+const numberAnimation = new NumberAnimation(".numeros span", ".numeros", "active")
+numberAnimation.init()
