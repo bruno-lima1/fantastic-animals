3c4c0a3fe4b0bd31fb084fcb59ea7f65dff5581c
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-12 10:38:06 -0300

    changing tab nav to object oriented

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,65 +1,27 @@
-export default function initTabNav() {
-  const images = document.querySelectorAll("[data-tab-menu] img");
-  const contents = document.querySelectorAll("[data-tab-content] li");
-  if (images.length && contents.length) {
-    contents[0].classList.add("active");
-    images.forEach((image, index) => {
+export default class TabNav {
+  constructor(image, contents, active) {
+    this.images = document.querySelectorAll(image);
+    this.contents = document.querySelectorAll(contents);
+    this.active = active;
+  }
+  showContent(index) {
+    this.contents.forEach((content) => {
+      content.classList.remove(this.active);
+    });
+    this.contents[index].classList.add(this.active);
+  }
+  addEvent() {
+    this.images.forEach((image, index) => {
       image.addEventListener("click", () => {
-        handleClick(index);
+        this.showContent(index);
       });
-      function handleClick(index) {
-        contents.forEach((content) => {
-          content.classList.remove("active");
-        });
-        contents[index].classList.add("active");
-      }
     });
   }
+  init() {
+    if (this.images.length && this.contents.length) {
+      this.showContent(0);
+      this.addEvent();
+    }
+    return this;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default class TabNav {
-//   constructor(images, contents) {
-//     this.images = document.querySelectorAll(images);
-//     this.contents = document.querySelectorAll(contents);
-//   }
-//   showContent(index) {
-//     this.contents.forEach((content) => {
-//       content.classList.remove("active");
-//     });
-//     this.contents[index].classList.add("active");
-//   }
-//   addEvent() {
-//     this.images.forEach((image, index) => {
-//       image.addEventListener("click", () => {
-//         this.showContent(index);
-//       });
-//     });
-//   }
-//   init() {
-//     if (this.images.length && this.contents.length) {
-//       this.showContent(0);
-//       this.addEvent()
-//     }
-//     return this;
-//   }
-// }
