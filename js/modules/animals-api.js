488b44044b42f357d26c93b9504143dbb6ba814d
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-15 15:56:03 -0300

    commenting lines

--- js/modules/animals-api.js
+++ js/modules/animals-api.js
@@ -42,9 +42,37 @@ export default function initAnimalsApi() {
 
 
 
-
-
-
 // import NumberAnimation from "./number-animation.js";
 // const numberAnimation = new NumberAnimation(".numeros span",".numeros","active");
 // numberAnimation.init();
+
+// import initNumberAnimation from "./number-animation.js";
+
+// export default function animalsApi(url, target) {
+//   const numberSection = document.querySelector(target);
+//   const createAnimals = async () => {
+//     try {
+//       const response = await fetch(url);
+//       const data = await response.json();
+//       data.forEach((values) => {
+//         fill(values)
+//       });
+//       numberAnimation()
+//     } catch (erro) {
+//       console.log(erro);
+//     }
+//   };
+//   const fill = (values) => {
+//     const animalSection = createTags(values)
+//     numberSection.appendChild(animalSection);
+//   }
+//   const numberAnimation = () => {
+//     initNumberAnimation();
+//   }
+//   const createTags = (values) => {
+//     const div = document.createElement("div");
+//     div.innerHTML = `<h3>${values.specie}</h3><span>${values.total}</span>`;
+//     return div;
+//   };
+//   return createAnimals()
+// }
