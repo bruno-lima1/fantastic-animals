3904f82e64e673a98a71b6feeba4f22c647dc5ec
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-17 17:12:39 -0300

    dropdown menu review

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,30 +1,74 @@
-import initOutsideClick from "./outsideclick.js";
+import initOutsideClick from "./outsideclick.js"
 
-export default class DropdownMenu {
-  constructor(menu, active, event) {
-    this.menu = document.querySelector(menu);
-    this.eventToggleMenu = this.eventToggleMenu.bind(this);
-    this.active = active;
-    if (event === undefined) this.event = "click";
-    else this.event = event
-  }
-  toggleMenu() {
-    this.menu.classList.toggle(this.active);
-    initOutsideClick(this.menu, () => {
-      this.menu.classList.remove(this.active);
-    });
-  }
-  eventToggleMenu(event) {
-    event.preventDefault();
-    this.toggleMenu()
-  }
-  addEvent() {
-    this.menu.addEventListener(this.event, this.eventToggleMenu);
-  }
-  init() {
-    if (this.menu) {
-      this.addEvent();
+export default function initDropdownMenu() {
+  const menu = document.querySelector("[data-dropdown-menu]");
+  if (menu) {
+    menu.addEventListener("click", handleClick);
+    function handleClick(event) {
+      event.preventDefault();
+      menu.classList.toggle("active");
+      initOutsideClick(menu, () => {
+      menu.classList.remove("active");
+      })
     }
-    return this;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import initOutsideClick from "./outsideclick.js";
+
+// export default class DropdownMenu {
+//   constructor(menu, active, event) {
+//     this.menu = document.querySelector(menu);
+//     this.eventToggleMenu = this.eventToggleMenu.bind(this);
+//     this.active = active;
+//     if (event === undefined) this.event = "click";
+//     else this.event = event
+//   }
+//   toggleMenu() {
+//     this.menu.classList.toggle(this.active);
+//     initOutsideClick(this.menu, () => {
+//       this.menu.classList.remove(this.active);
+//     });
+//   }
+//   eventToggleMenu(event) {
+//     event.preventDefault();
+//     this.toggleMenu()
+//   }
+//   addEvent() {
+//     this.menu.addEventListener(this.event, this.eventToggleMenu);
+//   }
+//   init() {
+//     if (this.menu) {
+//       this.addEvent();
+//     }
+//     return this;
+//   }
+// }
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import bitcoinApi from "./modules/bitcoin-api.js";
 import initAnimalsApi from "./modules/animals-api.js";
 import initScrollAnimation from "./modules/scroll-animation.js";
 import initAccordionNav from "./modules/accordion-nav.js";
-// import initDropdownMenu from "./modules/dropdown-menu.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 import initTimeOperation from "./modules/time-operation.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 
@@ -15,15 +15,12 @@ tabNav.init()
 import Modal from "./modules/modal.js";
 const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]", "active");
 modal.init()
-import DropdownMenu from "./modules/dropdown-menu.js";
-const dropdownMenu = new DropdownMenu("[data-dropdown-menu]", "active")
-dropdownMenu.init()
 
 bitcoinApi("https://blockchain.info/ticker", "[data-bitcoin]");
 initAnimalsApi()
 initScrollAnimation();
 initAccordionNav();
-// initDropdownMenu();
+initDropdownMenu();
 initTimeOperation();
 initMenuMobile();
 
@@ -44,9 +41,12 @@ initMenuMobile();
 
 
 
-
 // import animalsApi from "./modules/animals-api.js";
 // animalsApi("./animals-api.json", "[data-number]")
+
+// import DropdownMenu from "./modules/dropdown-menu.js";
+// const dropdownMenu = new DropdownMenu("[data-dropdown-menu]", "active")
+// dropdownMenu.init()
 
 // import ScrollAnimation from "./modules/scroll-animation.js";
 // const scrollAnimation = new ScrollAnimation("[data-scroll-animation]", "active");
