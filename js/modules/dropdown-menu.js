54503d912ee25a3ef3d38e6312a16da28b502177
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-17 17:10:52 -0300

    dropdown menu review

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,15 +1,30 @@
-import initOutsideClick from "./outsideclick.js"
+import initOutsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const menu = document.querySelector("[data-dropdown-menu]");
-  if (menu) {
-    menu.addEventListener("click", handleClick);
-    function handleClick(event) {
-      event.preventDefault();
-      menu.classList.toggle("active");
-      initOutsideClick(menu, () => {
-      menu.classList.remove("active");
-      })
+export default class DropdownMenu {
+  constructor(menu, active, event) {
+    this.menu = document.querySelector(menu);
+    this.eventToggleMenu = this.eventToggleMenu.bind(this);
+    this.active = active;
+    if (event === undefined) this.event = "click";
+    else this.event = event
+  }
+  toggleMenu() {
+    this.menu.classList.toggle(this.active);
+    initOutsideClick(this.menu, () => {
+      this.menu.classList.remove(this.active);
+    });
+  }
+  eventToggleMenu(event) {
+    event.preventDefault();
+    this.toggleMenu()
+  }
+  addEvent() {
+    this.menu.addEventListener(this.event, this.eventToggleMenu);
+  }
+  init() {
+    if (this.menu) {
+      this.addEvent();
     }
+    return this;
   }
 }
