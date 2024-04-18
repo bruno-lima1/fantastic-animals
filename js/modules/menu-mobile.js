1166cda785383e4f6d49163652cfb1c7a4797ba8
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-18 10:56:56 -0300

    fixing menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,29 +1,73 @@
 import initOutsideClick from "./outsideclick.js";
 
-export default class MenuMobile {
-  constructor(button, menu, active, event) {
-    this.button = document.querySelector(button);
-    this.menu = document.querySelector(menu);
-    this.active = active;
-    if (event === undefined) this.event = "click";
-    else this.event = event;
-    this.activeMenu = this.activeMenu.bind(this)
-  }
-  activeMenu() {
-    this.button.classList.add(this.active);
-    this.menu.classList.add(this.active);
-    initOutsideClick(this.menu, () => {
-      this.button.classList.remove(this.active);
-      this.menu.classList.remove(this.active);
-    });
-  }
-  addEvent() {
-    this.button.addEventListener(this.event, this.activeMenu);
-  }
-  init() {
-    if (this.button && this.menu) {
-      this.addEvent()
+export default function initMenuMobile() {
+  const button = document.querySelector("[data-menu-mobile]");
+  const menu = document.querySelector("[data-menu-list]");
+  if (button && menu) {
+    button.addEventListener("click", handleClick);
+    function handleClick() {
+      button.classList.add("active");
+      menu.classList.add("active");
+      initOutsideClick(menu, () => {
+        button.classList.remove("active");
+        menu.classList.remove("active");
+      });
     }
-    return this;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import initOutsideClick from "./outsideclick.js";
+
+// export default class MenuMobile {
+//   constructor(button, menu, active, event) {
+//     this.button = document.querySelector(button);
+//     this.menu = document.querySelector(menu);
+//     this.active = active;
+//     if (event === undefined) this.event = "click";
+//     else this.event = event;
+//     this.activeMenu = this.activeMenu.bind(this)
+//   }
+//   activeMenu() {
+//     this.button.classList.add(this.active);
+//     this.menu.classList.add(this.active);
+//     initOutsideClick(this.menu, () => {
+//       this.button.classList.remove(this.active);
+//       this.menu.classList.remove(this.active);
+//     });
+//   }
+//   addEvent() {
+//     this.button.addEventListener(this.event, this.activeMenu);
+//   }
+//   init() {
+//     if (this.button && this.menu) {
+//       this.addEvent()
+//     }
+//     return this;
+//   }
+// }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import bitcoinApi from "./modules/bitcoin-api.js";
 import initAnimalsApi from "./modules/animals-api.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
-// import initMenuMobile from "./modules/menu-mobile.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 import initScrollAnimation from "./modules/scroll-animation.js";
 import initAccordionNav from "./modules/accordion-nav.js";
 import initTimeOperation from "./modules/time-operation.js";
@@ -15,14 +15,11 @@ tabNav.init()
 import Modal from "./modules/modal.js";
 const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]", "active");
 modal.init()
-import MenuMobile from "./modules/menu-mobile.js";
-const menuMobile = new MenuMobile("[data-menu-mobile]", "[data-menu-list]", "active");
-menuMobile.init()
 
 bitcoinApi("https://blockchain.info/ticker", "[data-bitcoin]");
 initAnimalsApi()
 initDropdownMenu();
-// initMenuMobile();
+initMenuMobile();
 initScrollAnimation();
 initAccordionNav();
 initTimeOperation();
@@ -51,6 +48,10 @@ initTimeOperation();
 // import DropdownMenu from "./modules/dropdown-menu.js";
 // const dropdownMenu = new DropdownMenu("[data-dropdown-menu]", "active")
 // dropdownMenu.init()
+
+// import MenuMobile from "./modules/menu-mobile.js";
+// const menuMobile = new MenuMobile("[data-menu-mobile]", "[data-menu-list]", "active");
+// menuMobile.init()
 
 // import ScrollAnimation from "./modules/scroll-animation.js";
 // const scrollAnimation = new ScrollAnimation("[data-scroll-animation]", "active");
