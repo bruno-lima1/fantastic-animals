048ee4ee731a68d52887b82d7f7eb9012006dda9
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-18 10:54:18 -0300

    menu mobile review

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 import bitcoinApi from "./modules/bitcoin-api.js";
 import initAnimalsApi from "./modules/animals-api.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
+// import initMenuMobile from "./modules/menu-mobile.js";
 import initScrollAnimation from "./modules/scroll-animation.js";
 import initAccordionNav from "./modules/accordion-nav.js";
 import initTimeOperation from "./modules/time-operation.js";
-import initMenuMobile from "./modules/menu-mobile.js";
 
 import SmoothScroll from "./modules/smooth-scroll.js";
 const smoothScroll = new SmoothScroll("[data-smooth-scroll] a[href^='#']");
@@ -15,14 +15,17 @@ tabNav.init()
 import Modal from "./modules/modal.js";
 const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]", "active");
 modal.init()
+import MenuMobile from "./modules/menu-mobile.js";
+const menuMobile = new MenuMobile("[data-menu-mobile]", "[data-menu-list]", "active");
+menuMobile.init()
 
 bitcoinApi("https://blockchain.info/ticker", "[data-bitcoin]");
 initAnimalsApi()
 initDropdownMenu();
+// initMenuMobile();
 initScrollAnimation();
 initAccordionNav();
 initTimeOperation();
-initMenuMobile();
 
 
 
