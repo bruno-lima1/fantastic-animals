955faf5c3b6a841b9e4cc92c8534ea4fc66982f8
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-24 13:11:23 -0300

    fixing indentation

--- js/modules/time-operation.js
+++ js/modules/time-operation.js
@@ -6,26 +6,26 @@ export default class TimeOperation {
   init() {
     if (this.timeOperation) {
       this.timeOpen();
-      this.currentTime();
-      this.calculateTimeOperation();
-      this.enableVerification();
     }
     return this;
   }
   timeOpen() {
     this.daysOpen = this.timeOperation.dataset.days.split(", ").map(Number);
     this.hoursOpen = this.timeOperation.dataset.hours.split(", ").map(Number);
+    return this.currentTime();
   }
   currentTime() {
     const currentDate = new Date();
     this.currentDay = currentDate.getDay();
     this.currentHour = currentDate.getUTCHours() - 3;
+    return this.calculateTimeOperation();
   }
   calculateTimeOperation() {
     this.dayIsOpen = this.daysOpen.indexOf(this.currentDay) !== -1;
     this.hourIsOpen =
       this.hoursOpen[0] <= this.currentHour &&
       this.hoursOpen[1] > this.currentHour;
+    return this.enableVerification();
   }
   enableVerification() {
     if (this.dayIsOpen && this.hourIsOpen) {
