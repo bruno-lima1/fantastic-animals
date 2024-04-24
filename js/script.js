3ce9b7af4ddf24670015002a6232a0a12a59c2d8
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-24 13:36:31 -0300

    updating script

--- js/script.js
+++ js/script.js
@@ -6,31 +6,9 @@ import AccordionNav from "./modules/accordion-nav.js";
 const accordionNav = new AccordionNav("[data-accordion-nav] button", "[data-accordion-nav] dd")
 accordionNav.init()
 
-import MenuMobile from "./modules/menu-mobile.js";
-const menuMobile = new MenuMobile("[data-menu-mobile]", "[data-menu-list]");
-menuMobile.init()
-
-import DropdownMenu from "./modules/dropdown-menu.js";
-const dropdownMenu = new DropdownMenu("[data-dropdown-menu]");
-dropdownMenu.init()
-
 import SmoothScroll from "./modules/smooth-scroll.js";
 const smoothScroll = new SmoothScroll("[data-smooth-scroll] a[href^='#']");
 smoothScroll.init()
-
-import Modal from "./modules/modal.js";
-const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]");
-modal.init()
-
-import TimeOperation from "./modules/time-operation.js";
-const timeOperation = new TimeOperation("[data-days]")
-timeOperation.init()
-
-import bitcoinApi from "./modules/bitcoin-api.js";
-bitcoinApi("https://blockchain.info/ticker", "[data-bitcoin]");
-
-import animalsApi from "./modules/animals-api.js";
-animalsApi("./animals-api.json", "[data-number]")
 
 
 
@@ -38,6 +16,28 @@ import initScrollAnimation from "./modules/scroll-animation.js";
 initScrollAnimation();
 
 
+
+import Modal from "./modules/modal.js";
+const modal = new Modal("[data-modal-open]", "[data-modal-close]", "[data-modal-container]");
+modal.init()
+
+import DropdownMenu from "./modules/dropdown-menu.js";
+const dropdownMenu = new DropdownMenu("[data-dropdown-menu]");
+dropdownMenu.init()
+
+import MenuMobile from "./modules/menu-mobile.js";
+const menuMobile = new MenuMobile("[data-menu-mobile]", "[data-menu-list]");
+menuMobile.init()
+
+import TimeOperation from "./modules/time-operation.js";
+const timeOperation = new TimeOperation("[data-days]")
+timeOperation.init()
+
+import animalsApi from "./modules/animals-api.js";
+animalsApi("./animals-api.json", "[data-number]")
+
+import bitcoinApi from "./modules/bitcoin-api.js";
+bitcoinApi("https://blockchain.info/ticker", "[data-bitcoin]");
 
 
 
