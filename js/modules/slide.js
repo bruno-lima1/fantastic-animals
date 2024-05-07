84aedb890f6e4b69b239c9042ff7c960c6f88420
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-07 13:48:21 -0300

    adding mobile

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,40 +1,52 @@
 export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
-    this.slide = document.querySelector(slide);
-    this.values = { startPosition: 0, movement: 0, endPosition: 0, distance: 0 };
+    this.slide = document.querySelector(slide)
+    this.values = { startPosition: 0, movement: 0, endPosition: 0, distance: 0}
   }
   init() {
     if (this.wrapper && this.slide) {
-      this.bindEvents();
-      this.addEvent();
+      this.bindEvents()
+      this.addEvents()
     }
     return this;
   }
   bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
+    this.onStart = this.onStart.bind(this)
+    this.onMove = this.onMove.bind(this)
+    this.onEnd = this.onEnd.bind(this)
   }
-  addEvent() {
-    this.wrapper.addEventListener("mousedown", this.onStart);
-    this.wrapper.addEventListener("mouseup", this.onEnd);
+  addEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart)
+    this.wrapper.addEventListener("touchstart", this.onStart)
+    this.wrapper.addEventListener("mouseup", this.onEnd)
+    this.wrapper.addEventListener("touchend", this.onEnd)
   }
   onStart(event) {
-    this.wrapper.addEventListener("mousemove", this.onMove);
-    this.values.startPosition = event.clientX;
+    let movetype;
+    if (event.type === "mousedown") {
+      this.values.startPosition = event.clientX
+      movetype = "mousemove"
+    } else {
+      this.values.startPosition = event.changedTouches[0].clientX
+      movetype = "touchmove"
+    }
+    this.wrapper.addEventListener(movetype, this.onMove)
   }
   onMove(event) {
     event.preventDefault()
-    this.values.movement = (this.values.startPosition - event.clientX) * 1.6;
-    this.values.distance = this.values.endPosition - this.values.movement
-    return this.moveSlide();
-  }
-  moveSlide() {
+    const clientX = (event.type === "mousemove")
+      ? event.clientX
+      : event.changedTouches[0].clientX
+    this.values.movement = Math.floor((this.values.startPosition - clientX) * 1.6);
+    this.values.distance = Math.floor(this.values.endPosition - this.values.movement)
     this.slide.style.transform = `translate3d(${this.values.distance}px, 0, 0)`;
   }
-  onEnd() {
-    this.wrapper.removeEventListener("mousemove", this.onMove);
-    this.values.endPosition = this.values.distance;
+  onEnd(event) {
+    const typemove = (event.type === "mouseup")
+      ? "mousemove"
+      : "touchmove"
+    this.wrapper.removeEventListener(typemove, this.onMove)
+    this.values.endPosition = this.values.distance
   }
 }
