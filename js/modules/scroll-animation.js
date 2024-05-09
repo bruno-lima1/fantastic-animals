5be46cc2ab8ff946461e4077ee681be13862718e
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-09 19:27:00 -0300

    adding animation

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -42,41 +42,41 @@
 
 
 
-// import debounce from "./debounce.js"
+import debounce from "./debounce.js"
 
-// export default class ScrollAnimation {
-//   constructor(section) {
-//     this.section = document.querySelectorAll(section)
-//     this.getOffsetTop = debounce(this.getOffsetTop.bind(this), 50)
-//     this.screenHalf = window.innerHeight * .7;
-//     this.active = "active"
-//   }
-//   init() {
-//     if (this.section.length) {
-//       this.addEvent()
-//     }
-//     return this;
-//   }
-//   addEvent() {
-//     window.addEventListener("scroll", this.getOffsetTop)
-//   }
-//   getOffsetTop() {
-//     const data = [...this.section].map((element) => {
-//       return {
-//         element,
-//         offsetTop: element.offsetTop - this.screenHalf
-//       }
-//     })
-//     return this.enableScrollAnimation(data)
-//   }
-//   enableScrollAnimation(data) {
-//     data.forEach((item) => {
-//       return window.scrollY > item.offsetTop
-//         ? item.element.classList.add(this.active)
-//         : item.element.classList.remove(this.active)
-//     })
-//   }
-//   stop() {
-//     window.removeEventListener("scroll", this.getOffsetTop)
-//   }
-// }
+export default class ScrollAnimation {
+  constructor(section) {
+    this.section = document.querySelectorAll(section)
+    this.getOffsetTop = debounce(this.getOffsetTop.bind(this), 50)
+    this.screenHalf = window.innerHeight * .7;
+    this.active = "active"
+  }
+  init() {
+    if (this.section.length) {
+      this.addEvent()
+    }
+    return this;
+  }
+  addEvent() {
+    window.addEventListener("scroll", this.getOffsetTop)
+  }
+  getOffsetTop() {
+    const data = [...this.section].map((element) => {
+      return {
+        element,
+        offsetTop: element.offsetTop - this.screenHalf
+      }
+    })
+    return this.enableScrollAnimation(data)
+  }
+  enableScrollAnimation(data) {
+    data.forEach((item) => {
+      return window.scrollY > item.offsetTop
+        ? item.element.classList.add(this.active)
+        : item.element.classList.remove(this.active)
+    })
+  }
+  stop() {
+    window.removeEventListener("scroll", this.getOffsetTop)
+  }
+}
--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,8 +1,13 @@
 export default class Slide {
   constructor(wrapper, slide) {
-    this.wrapper = document.querySelector(wrapper);
+    this.wrapper = document.querySelector(wrapper)
     this.slide = document.querySelector(slide)
-    this.values = { startPosition: 0, movement: 0, distance: 0, endPosition: 0 }
+    this.values = {
+      startPosition: 0,
+      movement: 0,
+      distance: 0,
+      endPosition: 0
+    }
   }
   init() {
     if (this.wrapper && this.slide) {
@@ -18,19 +23,19 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this)
   }
   addEvents() {
-    this.wrapper.addEventListener("mousedown", this.onStart);
-    this.wrapper.addEventListener("touchstart", this.onStart);
+    this.wrapper.addEventListener("mousedown", this.onStart)
+    this.wrapper.addEventListener("touchstart", this.onStart)
     this.wrapper.addEventListener("mouseup", this.onEnd)
     this.wrapper.addEventListener("touchend", this.onEnd)
   }
   onStart(event) {
-    let movetype;
+    let movetype
     if (event.type === "mousedown") {
-      this.values.startPosition = event.clientX;
       movetype = "mousemove"
-    } else {
-      this.values.startPosition = event.changedTouches[0].clientX
+      this.values.startPosition = event.clientX
+    } else if (event.type === "touchstart") {
       movetype = "touchmove"
+      this.values.startPosition = event.changedTouches[0].clientX
     }
     this.wrapper.addEventListener(movetype, this.onMove)
   }
@@ -39,8 +44,8 @@ export default class Slide {
     const clientX = (event.type === "mousemove")
       ? event.clientX
       : event.changedTouches[0].clientX
-    this.values.movement = (this.values.startPosition - clientX) * 1.6;
-    this.values.distance = this.values.endPosition - this.values.movement;
+    this.values.movement = this.values.startPosition - clientX
+    this.values.distance = this.values.endPosition - this.values.movement
     return this.moveSlide(this.values.distance)
   }
   moveSlide(distance) {
@@ -52,21 +57,48 @@ export default class Slide {
       : "touchmove"
     this.wrapper.removeEventListener(movetype, this.onMove)
     this.values.endPosition = this.values.distance
+    return this.changeSlideOnEnd()
   }
   slideConfig() {
-    this.slideArray = [...this.slide.children].map((image) => {
-      const position = this.getPosition(image)
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.getPosition(element)
       return {
         position
       }
     })
   }
-  getPosition(image) {
-    const margin = (this.wrapper.offsetWidth - image.offsetWidth) / 2
-    return -(image.offsetLeft - margin)
+  getPosition(element) {
+    const margin = (this.wrapper.offsetWidth - element.offsetWidth) / 2
+    return -(element.offsetLeft - margin)
   }
   changeSlide(index) {
     this.moveSlide(this.slideArray[index].position)
     this.values.endPosition = this.slideArray[index].position
+    this.slideIndexNav(index)
+  }
+  slideIndexNav(index) {
+    const last = this.slideArray.length - 1
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1
+    }
+  }
+  changeSlideOnEnd() {
+    if (this.values.movement > 1) {
+      this.enableNextSlide()
+    } else if (this.values.movement < -1) {
+      this.enablePrevSlide()
+    }
+  }
+  enableNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next)
+    }
+  }
+  enablePrevSlide() {
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev)
+    }
   }
 }
