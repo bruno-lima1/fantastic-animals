bed5cfc4cd621b181a363d8c446bf894dd0f5a3f
Bruno César <brunocesar.tenorio1@gmail.com>
2024-06-11 23:04:57 -0300

    updating slide

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,11 +1,13 @@
+import debounce from "./debounce.js";
+
 export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide)
     this.values = {
       startPosition: 0,
-      movement: 0,
       distance: 0,
+      movePosition: 0,
       endPosition: 0
     }
     this.active = "active"
@@ -13,11 +15,8 @@ export default class Slide {
   init() {
     if (this.wrapper && this.slide) {
       this.bindEvents()
-      this.slideEvents()
-      this.getPosition()
+      this.addSlideEvents()
       this.centralizeSlide(0)
-      this.highlightMainImage()
-      this.resizeEvent()
     }
     return this;
   }
@@ -25,101 +24,86 @@ export default class Slide {
     this.onStart = this.onStart.bind(this)
     this.onMove = this.onMove.bind(this)
     this.onEnd = this.onEnd.bind(this)
-    this.onResize = this.onResize.bind(this)
+    this.centralizeWhenResize = debounce(this.centralizeWhenResize.bind(this), 1000)
   }
-  slideEvents() {
+  addSlideEvents() {
     this.wrapper.addEventListener("mousedown", this.onStart)
     this.wrapper.addEventListener("touchstart", this.onStart)
     this.wrapper.addEventListener("mouseup", this.onEnd)
     this.wrapper.addEventListener("touchend", this.onEnd)
+    window.addEventListener("resize", this.centralizeWhenResize)
   }
   onStart(event) {
-    let movetype
+    event.preventDefault()
     if (event.type === "mousedown") {
-      event.preventDefault()
-      movetype = "mousemove"
+      this.wrapper.addEventListener("mousemove", this.onMove)
       this.values.startPosition = event.clientX
-    } else if (event.type === "touchstart") {
-      movetype = "touchmove"
+    } else {
+      this.wrapper.addEventListener("touchmove", this.onMove)
       this.values.startPosition = event.changedTouches[0].clientX
     }
-    this.wrapper.addEventListener(movetype, this.onMove)
-    return this.transition(false)
+    this.enableTransition(false)
   }
   onMove(event) {
-    const clientX = event.type === "mousemove"
-      ? event.clientX
-      : event.changedTouches[0].clientX
-    this.values.movement = (this.values.startPosition - clientX) * 1.6
-    this.values.distance = this.values.endPosition - this.values.movement
-    return this.moveSlide(this.values.distance)
+    if (event.type === "mousemove") {
+      this.values.distance = this.values.startPosition - event.clientX
+    } else {
+      this.values.distance = this.values.startPosition - event.changedTouches[0].clientX
+    }
+    this.values.movePosition = this.values.endPosition - this.values.distance
+    this.moveSlide(this.values.movePosition)
   }
-  moveSlide(distance) {
-    this.slide.style.transform = `translate3d(${distance}px, 0, 0)`
+  moveSlide(position) {
+    this.slide.style.transform = `translate3d(${position}px, 0, 0)`
   }
-  onEnd(event) {
-    const movetype = event.type === "mouseup"
-      ? "mousemove"
-      : "touchmove"
-    this.wrapper.removeEventListener(movetype, this.onMove)
-    this.values.endPosition = this.values.distance
+  onEnd() {
+    this.wrapper.removeEventListener("mousemove", this.onMove)
+    this.values.endPosition = this.values.movePosition
     this.changeSlide()
-    this.transition(true)
-    return this.highlightMainImage()
-  }
-  getPosition() {
-    this.slideArray = [...this.slide.children].map((element) => {
-      const margin = (this.wrapper.offsetWidth - element.offsetWidth) / 2
-      const position = -(element.offsetLeft - margin)
-      return {
-        element,
-        position
-      }
-    })
+    this.enableTransition(true)
   }
   centralizeSlide(index) {
-    this.moveSlide(this.slideArray[index].position)
-    this.values.endPosition = this.slideArray[index].position
-    return this.slideIndex(index)
+    this.slideArray = [...this.slide.children].map((element) => {
+      const margin = (this.wrapper.offsetWidth - element.offsetWidth) / 2
+      const offsetLeft = -(element.offsetLeft - margin)
+      return {
+        element,
+        offsetLeft
+      }
+    })
+    this.moveSlide(this.slideArray[index].offsetLeft)
+    this.values.endPosition = this.slideArray[index].offsetLeft
+    this.indexSlide(index)
+    this.enableAnimation(index)
   }
-  slideIndex(index) {
-    this.positions = {
+  indexSlide(index) {
+    this.index = {
       prev: index - 1 === -1 ? undefined : index - 1,
       active: index,
-      next: index + 1 === 6 ? undefined : index + 1
+      next: index + 1 === 6 ? undefined : index + 1,
     }
   }
   changeSlide() {
-    return this.values.movement > 0
-      ? this.changeNextSlide()
-      : this.changePrevSlide()
+    if (this.values.distance > 0 && this.index.next !== undefined) {
+      this.centralizeSlide(this.index.next)
+    } else if (this.values.distance < 0 && this.index.prev !== undefined) {
+      this.centralizeSlide(this.index.prev)
+    } else if (this.values.distance > 0 && this.index.next === undefined) {
+      this.centralizeSlide(this.index.active)
+    } else if (this.values.distance < 0 && this.index.prev === undefined) {
+      this.centralizeSlide(this.index.active)
+    }
   }
-  changeNextSlide() {
-    return this.positions.next === undefined
-      ? this.centralizeSlide(this.positions.active)
-      : this.centralizeSlide(this.positions.next)
+  enableTransition(active) {
+    this.slide.style.transition = active ? ".5s" : "";
   }
-  changePrevSlide() {
-    return this.positions.prev === undefined
-      ? this.centralizeSlide(this.positions.active)
-      : this.centralizeSlide(this.positions.prev)
-  }
-  transition(active) {
-    this.slide.style.transition = active ? ".3s" : ""
-  }
-  highlightMainImage() {
-    this.slideArray.forEach((image) => {
-      image.element.classList.remove(this.active)
+  enableAnimation(index) {
+    this.slideArray.forEach((item) => {
+      item.element.classList.remove("active")
     })
-    this.slideArray[this.positions.active].element.classList.add(this.active)
+    this.slideArray[index].element.classList.add(this.active)
   }
-  resizeEvent() {
-    window.addEventListener("resize", this.onResize)
-  }
-  onResize() {
-    setTimeout(() => {
-      this.getPosition()
-      this.centralizeSlide(this.positions.active)
-    }, 1000)
+  centralizeWhenResize() {
+    this.centralizeSlide(this.index.active)
   }
 }
